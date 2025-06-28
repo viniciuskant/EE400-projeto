fd0a63fdb91742d81036ddc69a69ea19977d8de1
Vinicius P M Miguel <viniciusp070204@gmail.com>
2025-06-28 17:44:52 -0300

    update tests

--- projeto_b/test.sh
+++ projeto_b/test.sh
@@ -6,12 +6,15 @@ THREADS=16
 # Valores de z0
 z0_vals=(
   "0.0,0.0"
-  "0.3,0.1"
-  "0.3,0.5"
   "0.5,0.5"
-  "0.7,0.7"
-  "-0.4,0.6"
-  "-0.8,0.2"
+  "0.355,0.337"
+  "-0.70176,-0.3842"
+  "-0.835,-0.2321"
+  "-0.8,0.156"
+  "0.326,0.71"
+  "0.45,0.1428"
+  "0.48,0.001"
+  "0.37,-0.1"
 )
 
 # Valores de maxIterations
@@ -32,7 +35,7 @@ max_iter_vals=(
 exponent_vals=(
   -1
   2
-  5
+  3
   7
   13
   37
@@ -42,6 +45,13 @@ exponent_vals=(
 
 # Diretório base de resultados
 BASE_DIR="resultados_combinados"
+
+# Verificar se o script foi chamado com o parâmetro --julia
+USE_JULIA=false
+if [[ "$1" == "--julia" ]]; then
+  USE_JULIA=true
+  shift
+fi
 
 echo "Iniciando testes extensivos combinando z0, exponent (d) e maxIterations..."
 for d in "${exponent_vals[@]}"; do
@@ -53,8 +63,13 @@ for d in "${exponent_vals[@]}"; do
       mkdir -p "$output_dir"
 
       echo "d=$d | z0=($z0) | maxIterations=$max_iter"
-      ./bin/mandelbrot --threads $THREADS --z0 "$z0" --maxIterations "$max_iter" --exponent "$d" \
-        > "$output_dir/output_maxIter_${max_iter}.txt"
+      if $USE_JULIA; then
+        ./bin/mandelbrot --threads $THREADS --julia "$z0" --maxIterations "$max_iter" --exponent "$d"  \
+          > "$output_dir/output_maxIter_${max_iter}.txt"
+      else
+        ./bin/mandelbrot --threads $THREADS --z0 "$z0" --maxIterations "$max_iter" --exponent "$d" \
+          > "$output_dir/output_maxIter_${max_iter}.txt"
+      fi
     done
   done
 done
